a5ddcf0e1969b41c22ed3b0954ff10afd3468499
tarcisiopaixao <tarcisio.santostj@hotmail.com>
2024-02-13 14:45:49 -0300

    Conclusão do meu projeto

--- meu-desafio-dio-java/src/App.java
+++ meu-desafio-dio-java/src/App.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Iniciando meu primeiro desafio em java básico");
+
+        Banco cliente = new Banco();
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Seja bem-vindo ao Banco TPS\nPara criar sua conta no Banco TPS");
+
+        System.out.println("Digite numero de incrição com 6 digitos:");
+        int numeroInscricao = terminal.nextInt();
+        System.out.println("");
+
+        System.out.println("Agora digite sua agência (xxxx-x):");
+        String agencia = terminal.next();
+        System.out.println("");
+
+        System.out.println("Agora digite seu Nome completo:");
+        terminal.nextLine();
+        String nomeCliente = terminal.nextLine();
+        System.out.println("");
+
+        System.out.println("Qual Valor deseja depositar inicialmente?");
+        Double saldo = terminal.nextDouble();
+        System.out.println("");
+
+        cliente.setAgencia(agencia);
+        cliente.setNome(nomeCliente);
+        cliente.setNumero(numeroInscricao);
+        cliente.setSaldo(saldo);
+
+        System.out.println("Resumo do cadastro \n");
+
+        System.out.printf("Numero de inscrição: %d \n", cliente.getNumero());
+        System.out.printf("Agencia:%s \n", cliente.getAgencia());
+        System.out.printf("Nome do cliente: %s\n", cliente.getNome());
+        System.out.printf("Saldo em conta: %2f\n", cliente.getSaldo());
+        System.out.println("");
+
+        System.out.printf(
+                "Olá Cliente %s, obrigado por criar uma conta em nosso Banco TPS, sua agência é %s, conta %d, e seu saldo de: %2f está disponivel para saque!",
+                nomeCliente, agencia, numeroInscricao, saldo);
+        terminal.close();
+
     }
 }
---
+++ meu-desafio-dio-java/src/Banco.java
@@ -0,0 +1,89 @@
+public class Banco {
+    private int numero;
+    private String agencia;
+    private String nome;
+    private Double saldo;
+
+    public Banco() {
+    }
+
+    public Banco(int numero, String agencia, String nome, Double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + numero;
+        result = prime * result + ((agencia == null) ? 0 : agencia.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((saldo == null) ? 0 : saldo.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Banco other = (Banco) obj;
+        if (numero != other.numero)
+            return false;
+        if (agencia == null) {
+            if (other.agencia != null)
+                return false;
+        } else if (!agencia.equals(other.agencia))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (saldo == null) {
+            if (other.saldo != null)
+                return false;
+        } else if (!saldo.equals(other.saldo))
+            return false;
+        return true;
+    }
+
+}
